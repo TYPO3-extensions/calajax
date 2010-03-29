6794c24ffd4e479904a19be36cbc50cd6ab16e5e
mario_matzulla <mario_matzulla@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-29 20:04:39 +0000

    added functionality to upload images through ajax and store the image value
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/calajax/trunk@31707 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,7 @@
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 $TYPO3_CONF_VARS['FE']['eID_include']['calajax'] = 'EXT:calajax/service/ajax.php';
+$TYPO3_CONF_VARS['FE']['eID_include']['cal_ajax_image'] = 'EXT:calajax/service/ajaxImageUpload.php';
 
 /* Default confirm location View */
 t3lib_extMgm::addService($_EXTKEY,  'cal_view' /* sv type */,  'tx_cal_custom_model' /* sv key */,
@@ -13,4 +14,6 @@ t3lib_extMgm::addService($_EXTKEY,  'cal_view' /* sv type */,  'tx_cal_custom_mo
 		'className' => 'tx_cal_ajax_view',
 	)
 );
+
+$GLOBALS ['TYPO3_CONF_VARS']['FE']['EXTCONF']['ext/cal/service/class.tx_cal_event_service.php']['addAdditionalField'][] = 'EXT:calajax/hooks/class.tx_cal_additionalFields.php:&tx_cal_additionalFields';
 ?>
---
+++ hooks/class.tx_cal_additionalFields.php
@@ -0,0 +1,45 @@
+<?php
+/***************************************************************
+ * Copyright notice
+ *
+ * (c) 2008 Christian Technology Ministries International Inc.
+ * All rights reserved
+ *
+ * This file is part of the Web-Empowered Church (WEC)
+ * (http://WebEmpoweredChurch.org) ministry of Christian Technology Ministries 
+ * International (http://CTMIinc.org). The WEC is developing TYPO3-based
+ * (http://typo3.org) free software for churches around the world. Our desire
+ * is to use the Internet to help offer new life through Jesus Christ. Please
+ * see http://WebEmpoweredChurch.org/Jesus.
+ *
+ * You can redistribute this file and/or modify it under the terms of the 
+ * GNU General Public License as published by the Free Software Foundation;
+ * either version 2 of the License, or (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This file is distributed in the hope that it will be useful for ministry,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the file!
+ ***************************************************************/
+
+
+class tx_cal_additionalFields {
+
+	function addAdditionalField(&$insertFields, &$eventService){
+		if($eventService->rightsObj->isAllowedTo('edit','event','image')){
+			if(is_array($eventService->controller->piVars['ajax_image'])){
+				$insertFields['image'] = implode(',',$eventService->controller->piVars['ajax_image']);
+			}
+		}
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/calajax/hooks/class.tx_cal_additionalFields.php']) {
+	include_once ($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/calajax/hooks/class.tx_cal_additionalFields.php']);
+}
+?>
